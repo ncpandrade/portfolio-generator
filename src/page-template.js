cc4269657717617702b69497b3a974fa4ca85aaf
Nadine <nadine_paez@hotmail.com>
2021-10-12 17:23:27 -0400

    set up basic html

---
+++ src/page-template.js
@@ -0,0 +1,20 @@
+//FUNCTION to receive command-line args and inserts them to HTML template literal
+const generatePage = (userName, githubName) => `
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Portfolio Demo</title>
+</head>
+
+<body>
+  <h1>${name}</h1>
+  <h2><a href="https://github.com/${github}">Github</a></h2>
+</body>
+</html>
+`;
+
+//module-export stmt in the source file
+module.exports = generatePage;
